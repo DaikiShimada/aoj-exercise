c383cfabbe22c43ea7712f97c5d6e59ad07550b1
daiki_shimada_mba <sheema.sheemap@gmail.com>
2016-10-31 20:45:41 +0900

    Fix bug

--- src/volume_0/0001_List_of_Top_3_Hills.py
+++ src/volume_0/0001_List_of_Top_3_Hills.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import six
 
 def top_k_sort(data, k=3, reverse=True):
     data.sort(reverse=True)
     return data[:k]
 
 def main():
-    data = map(int, raw_input().split())
+    data = []
+    for line in sys.stdin:
+        data.append(int(line))
 
-    data = [int(v) for v in argv[1:]]
     for h in top_k_sort(data):
         print(h)
 
